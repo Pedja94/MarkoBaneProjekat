ff928e9eb6f92e26da2a387a5a8c4438b041ce15
Bane <nikolic.predrag94@outlook.com>
2017-07-09 23:49:36 +0200

    Skrol

--- Business/Program.cs
+++ Business/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.DTO;
+using Business.DataAccess;
 
 namespace Business
 {
@@ -10,7 +12,7 @@ namespace Business
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
